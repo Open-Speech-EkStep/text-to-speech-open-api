49b7733ead7f972ec0cc647fe13981f640fdb16e
soujyo <soujyo.sen@thoughtworks.com>
2022-01-05 01:29:01 +0530

    Soujyo | Dockerize initial version of fast api

--- dependencies/installer.sh
+++ dependencies/installer.sh
@@ -5,14 +5,14 @@ echo "Setting up model dependencies...."
 apt-get update && apt-get upgrade -y
 apt-get install -y sudo wget python3-pip git
 
-
+apt install python-is-python3
 git clone https://github.com/Open-Speech-EkStep/vakyansh-tts
 cd vakyansh-tts
 pip3 install --no-cache-dir -r requirement.txt
 bash install.sh
-python setup.py bdist_wheel
+python3 setup.py bdist_wheel
 pip install -e .
 cd tts_infer
 gsutil -m cp -r gs://vakyaansh-open-models/translit_models .
 
-apt install python-is-python3
+
