882f8ead3da6bffafd354f1a5e622b5ecadab934
soujyo <soujyo.sen@thoughtworks.com>
2022-01-05 13:05:52 +0530

    Soujyo | Update server ip

--- server.py
+++ server.py
@@ -81,5 +81,5 @@ async def tts(input: TextJson):
 
 if __name__ == "__main__":
     uvicorn.run(
-        "server:app", host="127.0.0.1", port=5000, log_level="info", reload=True
+        "server:app", host="0.0.0.0", port=5000, log_level="info", reload=True
     )
