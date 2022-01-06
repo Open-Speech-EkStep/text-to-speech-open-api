e26795f9dd12235aabfa3230e8e0a2661dc627a5
soujyo <soujyo.sen@thoughtworks.com>
2022-01-06 14:13:01 +0530

    Soujyo | Add paragraph mode

--- server.py
+++ server.py
@@ -2,16 +2,19 @@ import base64
 import json
 import os
 from typing import Optional
-
+import io
+import numpy as np
 import torch
 import uvicorn
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from indicnlp.tokenize import sentence_tokenize
+from mosestokenizer import *
 from pydantic import BaseModel
-from scipy.io.wavfile import write
 from tts_infer.num_to_word_on_sent import normalize_nums
 from tts_infer.transliterate import XlitEngine
 from tts_infer.tts import TextToMel, MelToWav
+from scipy.io.wavfile import read, write
 
 app = FastAPI()
 app.add_middleware(
@@ -29,6 +32,8 @@ class TextJson(BaseModel):
     gender: Optional[str] = "male"
 
 
+max_text_limit = 450
+INDIC = ["as", "bn", "gu", "hi", "kn", "ml", "mr", "or", "pa", "ta", "te"]
 MODEL_BASE_PATH = os.environ.get('models_base_path', '')
 gpu_present = torch.cuda.is_available()
 print("Gpu present : ", gpu_present)
@@ -51,22 +56,44 @@ for language_code, lang_config in model_config.items():
         MelToWav(hifi_model_dir=MODEL_BASE_PATH + lang_config.get("female_hifi"), device=DEVICE)]
 
 transliterate_obj = XlitEngine()
-# glow_hi_male = TextToMel(glow_model_dir="", device="")
-# glow_hi_female = TextToMel(glow_model_dir="", device="")
-# hifi_hi = MelToWav(hifi_model_dir="", device="")
-
-# available_choice = {
-#     "hi_male": [glow_hi_male, hifi_hi],
-#     "hi_female": [glow_hi_female, hifi_hi],
-# }
 
 print(available_choice)
+
+
+def split_sentences(paragraph, language):
+    if language == "en":
+        with MosesSentenceSplitter(language) as splitter:
+            return splitter([paragraph])
+    elif language in INDIC:
+        return sentence_tokenize.sentence_split(paragraph, lang=language)
 
 
 def pre_process_text(text, lang):
     if lang == 'hi':
         text = text.replace('।', '.')  # only for hindi models
     return text
+
+
+def run_tts(text, lang, t2s):
+    text_num_to_word = normalize_nums(text, lang)  # converting numbers to words in lang
+    text_num_to_word_and_transliterated = transliterate_obj.translit_sentence(text_num_to_word,
+                                                                              lang)  # transliterating english words to lang
+    mel = t2s[0].generate_mel(' ' + text_num_to_word_and_transliterated)
+    audio, sr = t2s[1].generate_wav(mel)
+    return audio, sr
+
+
+def run_tts_paragraph(text, lang, t2s):
+    audio_list = []
+    split_sentences_list = split_sentences(text, language='hi')
+
+    for sent in split_sentences_list:
+        audio, sr = run_tts(sent, lang, t2s)
+        audio_list.append(audio)
+
+    concatenated_audio = np.concatenate([i for i in audio_list])
+    # write(filename='temp_long.wav', rate=sr, data=concatenated_audio)
+    return concatenated_audio, sr
 
 
 @app.post("/TTS/")
@@ -84,13 +111,21 @@ async def tts(input: TextJson):
         )
 
     if text:
-        text = pre_process_text(text, lang)
-        text_num_to_word = normalize_nums(text, lang)  # converting numbers to words in lang
-        text_num_to_word_and_transliterated = transliterate_obj.translit_sentence(text_num_to_word,
-                                                                                  lang)  # transliterating english words to lang
-        mel = t2s[0].generate_mel(' ' + text_num_to_word_and_transliterated)
-        data, sr = t2s[1].generate_wav(mel)
-        write(filename='out.wav', rate=sr, data=data)
+        # text = pre_process_text(text, lang)
+        if len(text) > max_text_limit:
+            print("Running in paragraph mode...")
+            audio, sr = run_tts_paragraph(text, lang, t2s)
+        else:
+            print("Running in text mode...")
+            audio, sr = run_tts(text, lang, t2s)
+        torch.cuda.empty_cache()
+        print(audio)
+        bytes_wav = bytes()
+        byte_io = io.BytesIO(bytes_wav)
+        write(byte_io, sr, audio)
+        encoded_bytes = base64.b64encode(byte_io.read())
+        encoded_string = encoded_bytes.decode()
+        # write(filename='out.wav', rate=sr, data=audio) #If persisting of files are needed
     else:
         raise HTTPException(status_code=400, detail={"error": "No text"})
 
@@ -98,9 +133,9 @@ async def tts(input: TextJson):
     # audio = open('out.wav', mode='rb')
     # return StreamingResponse(audio, media_type="audio/wav")
 
-    with open("out.wav", "rb") as audio_file:
-        encoded_bytes = base64.b64encode(audio_file.read())
-        encoded_string = encoded_bytes.decode()
+    # with open("out.wav", "rb") as audio_file:
+    #     encoded_bytes = base64.b64encode(audio_file.read())
+    #     encoded_string = encoded_bytes.decode()
     return {"encoding": "base64", "data": encoded_string, "sr": sr}
 
 
