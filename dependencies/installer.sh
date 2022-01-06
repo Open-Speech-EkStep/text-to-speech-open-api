a5717f3c4bcb72d60edbcdaac23f62032669500b
soujyo <soujyo.sen@thoughtworks.com>
2022-01-06 14:18:09 +0530

    Soujyo | Add revised dependencies

--- dependencies/installer.sh
+++ dependencies/installer.sh
@@ -8,7 +8,7 @@ apt-get install -y sudo wget python3-pip git libsndfile1 libsndfile1-dev
 apt install python-is-python3
 git clone https://github.com/Open-Speech-EkStep/vakyansh-tts
 cd vakyansh-tts
-pip3 install --no-cache-dir -r requirement.txt
+pip3 install --no-cache-dir -r requirements.txt
 bash install.sh
 python3 setup.py bdist_wheel
 pip install -e .
